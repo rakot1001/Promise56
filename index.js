afaf58ecfc3342e63f418a53944282e9f944b4ec
Vlad <vladdp70@gmail.com>
2021-06-14 01:11:51 +0300

    Кот шредингера на промисах

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-// "use strict";
+"use strict";
 
-// fetch("./data.json") //подгрузи этот файл
-//   .then((response) => response.json()) // когда подгрузится - исполни эту функцию
-//   .then((data) => console.table(data)) // когда распарсишь запусти эту функцию
-//   .catch((error) => console.log("Error")); // проверь на ошибки
-
-// const firstPromise = new Promise(executor);
-
-// function executor(resolve, reject) {
-//   resolve();
-// }
-
-
-function delay(ms) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-delay(1500).then(() => {
-  console.log("timer stop");
+const catPromise = new Promise((resolve, reject) => {
+  const catDestiny = Math.random();
+  if (catDestiny >= 0.5) {
+    resolve("Cat is Alive");
+  } else {
+    reject("Cat is dead");
+  }
 });
+
+catPromise.then(checkCat).catch(checkCat);
+
+function checkCat(str) {
+  console.log("Status", str);
+}
