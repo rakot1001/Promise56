de7e72b22e82d3812b5b43e4a9cd322cd3c40d86
Vlad <vladdp70@gmail.com>
2021-06-14 00:25:50 +0300

    Правильный вид промисов

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 "use strict";
 
-const p = fetch("./data.json"); // когда загрузишь
-
-const jsonPromise = p.then((response) => {
-  // потом запусти эту функцию
-  return response.json();
-});
-
-
-jsonPromise.then((data) => {
-  console.table(data);
-});
+fetch("./data.json")//подгрузи этот файл
+  .then((response) => response.json())// когда подгрузится - исполни эту функцию
+  .then((data) => console.table(data));// когда распарсишь запусти эту функцию
