1535d354cfecc61bf00e166103ee2d368336e730
Vlad <vladdp70@gmail.com>
2021-06-14 00:41:49 +0300

    Обработчик ошибок catch

--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 
 fetch("./data.json")//подгрузи этот файл
   .then((response) => response.json())// когда подгрузится - исполни эту функцию
-  .then((data) => console.table(data));// когда распарсишь запусти эту функцию
+  .then((data) => console.table(data))  // когда распарсишь запусти эту функцию
+  .catch((error) => console.log('Error'));
