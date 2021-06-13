83d363ac350ce7c75d89ef3c9e55a164856b47ca
Vlad <vladdp70@gmail.com>
2021-06-14 00:58:44 +0300

    Конструктор для промисов

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-"use strict";
+// "use strict";
 
-fetch("./data.json")//подгрузи этот файл
-  .then((response) => response.json())// когда подгрузится - исполни эту функцию
-  .then((data) => console.table(data))  // когда распарсишь запусти эту функцию
-  .catch((error) => console.log('Error'));
+// fetch("./data.json") //подгрузи этот файл
+//   .then((response) => response.json()) // когда подгрузится - исполни эту функцию
+//   .then((data) => console.table(data)) // когда распарсишь запусти эту функцию
+//   .catch((error) => console.log("Error")); // проверь на ошибки
+
+const firstPromise = new Promise(executor);
+
+function executor(resolve, reject) {
+  resolve();
+}
