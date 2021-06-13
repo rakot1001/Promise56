ed297f958057e36d16e7452dcfc50a653b6f7e3a
Vlad <vladdp70@gmail.com>
2021-06-14 01:55:31 +0300

    Пару эксперементов с тестами по js

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 "use strict";
 
-function loadImage(src) {
-  const img = document.createElement("img");
-
-  img.setAttribute("src", src); // Установка атрибута src инициализирует загрузку картинки
-  return new Promise((resolve, reject) => {
-    img.addEventListener("load", () => {
-      resolve(img);
-    });
-    img.addEventListener("error", () => {
-      reject(new Error("Img isnt Load"));
-    });
-  });
+function sayH() {
+  setTimeout(() => alert("Hello"), 0);
 }
+clearTimeout(sayH);
 
-loadImage(
-  "https://i.pinimg.com/originals/35/82/21/358221b85dc0c666cbd6bf4961a260db.jpg"
-)
-  .then((img) => {
-    document.body.append(img);
-  })
-  .catch((err) => {
-    alert();
-  });
+console.log(2**3**2);
+const user = { name: "Джон", go() { alert(this.name) }, }
+
