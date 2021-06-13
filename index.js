70caacb96481aa6e490951fa3ee24d2ab2f8f64f
Vlad <vladdp70@gmail.com>
2021-06-13 22:02:20 +0300

    Работа с JSON

--- index.js
+++ index.js
@@ -1,37 +1,25 @@
 "use strict";
 
-// const btn = document.getElementById("btn");
+const user = {
+  firstName: "Vlad",
+  lastName: "Testovich",
+  age: 45,
+  getName() {
+    return this.firstName;
+  },
+  isSelected: null,
+  isAgree: false,
+  isMale: true,
+  [Symbol("test")]: {},
+  phones: ["111111111111111", "22222222222", "33333333333"],
+  friend: {
+    firstName: "1",
+    lastName: "2",
+  },
+};
 
-// btn.addEventListener("click", handleBtnClick);
+const serializedObject = JSON.stringify(user);
+console.log(serializedObject);
 
-// function handleBtnClick(evet) {
-//   for (let i = 0; i < 1000; i++) {}
-// }
-
-// setTimeout(() => {
-//   console.log("ok");
-// }, 1500);
-
-// function sum() {
-//   return 2 + 2;
-// }
-
-// console.log(sum());
-
-
-
-/* Функция последовательно выводит в консоль числа от 1 дл 20 с интервалом в 100мс Померять время исполнения */
-console.time('1');
-count();
-function count() {
-  let i = 1;
-   const id = setInterval(()=> {
-    console.log(i++);
-    if(i>20){
-      clearInterval(id);
-      console.timeEnd('1');
-    }
-  }, 100);
-  }
-
-
+const recoveredObject = JSON.parse(serializedObject);
+console.log(recoveredObject);
